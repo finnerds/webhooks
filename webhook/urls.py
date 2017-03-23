b8324d718a158d3f8ccc0e9a4bff54948997644f
Peter Rosemann <dkdndes@gmail.com>
2017-03-23 08:49:41 +0100

    missing , and added url for openid

--- webhook/urls.py
+++ webhook/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^webhook/$', views.github_ping, name='github_ping')
+    url(r'^webhook/$', views.github_ping, name='github_ping'),
+    url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 ]
