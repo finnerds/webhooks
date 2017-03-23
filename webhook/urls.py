b00a4e7b2be534e6b9e4348618c7b59f53e98a4e
Peter Rosemann <dkdndes@gmail.com>
2017-03-23 08:57:04 +0100

    added github_ping and oidp urls

--- webhook/urls.py
+++ webhook/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^webhook/$', views.github_ping, name='github_ping'),
+    url(r'^github_ping/$', views.github_ping, name='github_ping'),
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 ]
