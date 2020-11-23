d36c23608b8100316908285fd7030c4872fe2436
Peter Rosemann <dkdndes@users.noreply.github.com>
2020-11-23 20:05:32 +0100

    refactor!: removed odic because its not related

--- webhook/urls.py
+++ webhook/urls.py
@@ -3,5 +3,4 @@ from .views import github_ping
 
 urlpatterns = [
     url(r'^github_ping/$', github_ping, name='webhook_github_ping'),
-    url(r'^openid/', include('oidc_provider.urls', namespace='webhook_oidc_provider')),
 ]
